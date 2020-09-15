678055d77857e959efdabdadd75e65bd4659927a
GabrielJ0shua <gabriel.naves@ufu.br>
2020-09-15 19:31:22 -0300

    iniciando bem

--- NLW/src/server.ts
+++ NLW/src/server.ts
@@ -1,2 +1,11 @@
 import express from 'express';
 
+const app = express();
+
+app.get('/users', (request, response)=> {
+    console.log('Listagem de usuários')
+
+    response.send('Hello Word');
+})
+
+app.listen(3333);
