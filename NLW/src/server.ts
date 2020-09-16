d2d4111f7af8ea11d7816ea8f401f91855bcd7f6
GabrielJ0shua <gabriel.naves@ufu.br>
2020-09-16 19:25:24 -0300

    até 25:20

--- NLW/src/server.ts
+++ NLW/src/server.ts
@@ -1,17 +1,34 @@
-import express from 'express';
+import express, { response } from 'express';
 
 const app = express();
+const users = [
+    'Joshua',
+    'Dautonico',
+    'Jorge',
+    'teste',
+    'teste2'
+];
 
 app.get('/users', (request, response)=> {
-    console.log('Listagem de usuários')
+    const search = String(request.query.serach);
+    const filteredUsers = search ? users.filter(user => user.includes(search)): users;
+    return response.json(users);
+});
 
-    response.json([
-        'Joshua',
-        'Dautonico',
-        'jorge',
-        'teste',
-        'teste2'
-    ]);
-})
+app.get ('/users/:id', (request, response)=> {
+    const id = Number(request.params.id);
+    const user = users[id];
+    return response.json(user);
+});
+
+app.post('/users', (resquest, response) =>{
+    const user = {
+        name: 'Joshua',
+        email:'akakakkinho@gg.com'
+    };
+
+    return response.json(user);
+});
+
 
 app.listen(3333);
