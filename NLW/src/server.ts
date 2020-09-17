6f06be68362f7e2d90ec5677bde755a61e294ed7
GabrielJ0shua <gabriel.naves@ufu.br>
2020-09-17 11:46:42 -0300

    até 45:48

--- NLW/src/server.ts
+++ NLW/src/server.ts
@@ -1,34 +1,8 @@
-import express, { response } from 'express';
+import express from 'express';
+import routes from './routes';
 
 const app = express();
-const users = [
-    'Joshua',
-    'Dautonico',
-    'Jorge',
-    'teste',
-    'teste2'
-];
 
-app.get('/users', (request, response)=> {
-    const search = String(request.query.serach);
-    const filteredUsers = search ? users.filter(user => user.includes(search)): users;
-    return response.json(users);
-});
-
-app.get ('/users/:id', (request, response)=> {
-    const id = Number(request.params.id);
-    const user = users[id];
-    return response.json(user);
-});
-
-app.post('/users', (resquest, response) =>{
-    const user = {
-        name: 'Joshua',
-        email:'akakakkinho@gg.com'
-    };
-
-    return response.json(user);
-});
-
-
+app.use(express.json());
+app.use(routes);
 app.listen(3333);
