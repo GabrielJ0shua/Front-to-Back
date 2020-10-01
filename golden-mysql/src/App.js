b20d71605d805ed06eb2a2f147b5b8f2d21532ed
GabrielJ0shua <gabriel.naves@ufu.br>
2020-10-01 15:36:44 -0300

    up grade people, upgrades

--- golden-mysql/src/App.js
+++ golden-mysql/src/App.js
@@ -1,25 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ <div className="App-header">
+ <div class="d-flex justify-content-center">
+            <div class="container">
+                <blockquote class="blockquote text-center">
+                    <h1>Bem Vindo :)</h1>
+                </blockquote>
+                <div class="container ">
+                    <form class="text-center" action="logar.php" method="POST" autocomplete="on">
+                        <label for="user">Usuário</label>
+                        <br></br>
+                        <input class="rounded" type="text" name="user" required size="20" maxlength="60" autocomplete="given-name" placeholder="Coloque o Apelido" autofocus></input>
+                        <br></br>
+                        <br></br>
+                        <label for="senha">Senha</label>
+                        <br></br>
+                        <input class="rounded" type="password" name="senha" required size="20" maxlength="60" placeholder="Agora sua senha"></input>
+                        <br></br>
+                        <br></br>
+                        <button class="btn btn-danger rounded" type="button" class="btn btn-danger">Entrar</button>
+                        <br></br>
+                    </form>
+                </div> 
+                <blockquote class="blockquote text-center">
+                    <h3>Não é cadastradro? <a href="index(cadastro).html">Registra-se</a></h3>
+                </blockquote>
+            </div> 
+        </div>    
+ </div>
+ </div>
   );
 }
 
